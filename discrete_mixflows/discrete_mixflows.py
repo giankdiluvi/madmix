fix jacobian dimensions

discrete_mixflows/discrete_mixflows.py
@@ -94,7 +94,7 @@ def flow(x,u,steps,lp,xi=np.pi/16,direction='fwd'):
          
     M=x.shape[0]
     ljs=np.zeros(x.shape)
-    if steps==0: return x,u,0
+    if steps==0: return x,u,np.zeros(x.shape[1])
     for t in range(steps):
         for m in range(M):
             m_idx = m if direction=='fwd'else M-m-1 # if in reverse, update starting from the end