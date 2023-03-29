051fdba9d099787e79b647900549fff0bc1b8462
giankdiluvi <giankdiluvi@gmail.com>
2023-03-29 16:16:10 -0700

    move logsumexp to new aux src file

--- discrete_mixflows/discrete_mixflows.py
+++ discrete_mixflows/discrete_mixflows.py
@@ -195,20 +195,6 @@ auxiliary functions
 ########################################
 """
 
-def LogSumExp(w):
-    """
-    LogSumExp trick
-    
-    inputs:
-        w : (N,d) array, exponents
-        
-    outputs:
-        w' : (d,) array, log(sum(exp(w)))
-    """
-    
-    wmax = np.amax(w,axis=0)
-    return wmax + np.log(np.sum(np.exp(w-wmax[np.newaxis,:]),axis=0))
-
     
 def cdf(x,prbs): 
     """
